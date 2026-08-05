fd5a63fe07ecd2321895eb001deba578aad19dd5
Jehadkawash <jehad234kawash@yahoo.com>
2026-08-05 13:06:28 +0300

    Add login page and protect routes

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
-  apiKey:            "AIzaSyCKMrH2E_GP_MYZJrhF4LbxC1LmtVGx3Co",
-  authDomain:        "brave-academy.firebaseapp.com",
-  projectId:         "brave-academy",
-  storageBucket:     "brave-academy.firebasestorage.app",
-  messagingSenderId: "862804404676",
-  appId:             "1:862804404676:web:871e3bb0796c354f1f5c91",
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
